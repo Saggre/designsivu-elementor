ba22de891a2562743f81ee10885b5def25e5c21e
Saggre <sakri.koskimes@joinment.com>
2019-02-07 18:13:32 +0100

    Initial committ

---
+++ widgets/lottie.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace LottieElementor\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/**
+ * Elementor Hello World
+ *
+ * Elementor widget for hello world.
+ *
+ * @since 1.0.0
+ */
+class Lottie extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'lottie';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Lottie Animation', 'lottie-elementor' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-posts-ticker';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
+	public function get_script_depends() {
+		return [ 'lottie-elementor' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => __( 'Content', 'lottie-elementor' ),
+			]
+		);
+
+		$this->add_control(
+			'json',
+			[
+				'label' => __( 'Lottie JSON', 'lottie-elementor' ),
+				'type'  => Controls_Manager::TEXTAREA,
+			]
+		);
+
+		$this->add_control(
+			'max_width',
+			[
+				'label'   => __( 'Max Width', 'lottie-elementor' ),
+				'type'    => \Elementor\Controls_Manager::NUMBER,
+				'min'     => 0,
+				'max'     => 999999,
+				'step'    => 1,
+				'default' => 256,
+			]
+		);
+
+		$this->add_control(
+			'max_height',
+			[
+				'label'   => __( 'Max Height', 'lottie-elementor' ),
+				'type'    => \Elementor\Controls_Manager::NUMBER,
+				'min'     => 0,
+				'max'     => 999999,
+				'step'    => 1,
+				'default' => 256,
+			]
+		);
+
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Style', 'lottie-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label'   => __( 'Alignment', 'lottie-elementor' ),
+				'type'    => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left'   => [
+						'title' => __( 'Left', 'lottie-elementor' ),
+						'icon'  => 'fa fa-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'lottie-elementor' ),
+						'icon'  => 'fa fa-align-center',
+					],
+					'right'  => [
+						'title' => __( 'Right', 'lottie-elementor' ),
+						'icon'  => 'fa fa-align-right',
+					],
+				],
+				'default' => 'center',
+				'toggle'  => true,
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+		$uid      = 'lottie_' . uniqid();
+
+		$max_width  = $settings['max_width'];
+		$max_height = $settings['max_height'];
+
+		echo '<div style="text-align:' . $settings['text_align'] . ';">';
+		echo '<div style="width: ' . $max_width . 'px; height: ' . $max_height . 'px; display: inline-block;" class="lottie-elementor-item" id="' . $uid . '">';
+		echo '</div>';
+		echo '</div>';
+
+		echo '<script>';
+		echo "var animationData = " . $settings['json'] . ";";
+		echo "lottie.loadAnimation({animationData: animationData, container: document.getElementById('" . $uid . "'), renderer: 'svg', loop: true, autoplay: true});";
+		echo '</script>';
+	}
+
+}
